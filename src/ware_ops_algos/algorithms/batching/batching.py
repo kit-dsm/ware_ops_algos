2a7cf454f37c4ea843f58938ee30e190b34abc31
j4n1k <janik.bischoff@gmx.de>
2026-07-02 11:54:50 +0200

    changed seed for RandomBatching to test dispatch

--- src/ware_ops_algos/algorithms/batching/batching.py
+++ src/ware_ops_algos/algorithms/batching/batching.py
@@ -99,7 +99,7 @@ class RandomBatching(PriorityBatching):
     """
     algo_name = "RandomBatching"
 
-    def __init__(self, pick_cart: PickCart, articles: Articles, seed=42):
+    def __init__(self, pick_cart: PickCart, articles: Articles, seed=41):
         super().__init__(pick_cart, articles)
 
         self.seed = seed
